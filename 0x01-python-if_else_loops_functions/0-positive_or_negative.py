d3991dcab7339d65e95da0dd3cc2bbd1e4a817fd
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-09-21 04:59:40 +0100

    Create 0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+import random
+number = random.randint(-10, 10) 
+for n in number
+if n > 0:
+     print("{} is positive".format(number))
+elif n == 0:
+     print("{} is zero".format(number))
+elif n < 0:
+     print("{} is negative".format(number))
